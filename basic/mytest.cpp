59cb0d35e41897460e5eceb23ec51dccb06236cc
ysh8361 <sea5727@naver.com>
2021-01-20 08:34:06 +0900

    commit

--- basic/mytest.cpp
+++ basic/mytest.cpp
@@ -1,14 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <uv.h>
+#include <thread>
+#include <iostream>
 
 int64_t counter = 0;
 
 void wait_for_a_while(uv_idle_t* handle) {
-    counter++;
-
-    if (counter >= 10e6)
-        uv_idle_stop(handle);
+    std::cout << "wait_for_a_while" << std::endl;
+    uv_idle_stop(handle);
+}
+void cb(uv_timer_t* handle){
+    std::cout << "timeout!!" << std::endl;
 }
 
 int main() {
@@ -16,12 +19,17 @@ int main() {
     uv_loop_init(loop);
 
 
-    uv_idle_t idler;
-    uv_idle_init(loop, &idler);
-    uv_idle_start(&idler, wait_for_a_while);
+    uv_timer_t timer;
+    uv_timer_init(loop, &timer);
+    uv_timer_start(&timer, cb, 0, 5000);
 
-    printf("MyTest.\n");
-    uv_run(loop, UV_RUN_ONCE);
+    while(1){
+        
+        int ret = uv_run(loop, UV_RUN_DEFAULT);
+        printf("MyTest.ret:%d\n", ret);
+        // std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    
 
     uv_loop_close(loop);
     free(loop);
